afd5740ebad3e74a8f998ed939f441654b1e53da
Tonči Marinac <toncimarinac@gmail.com>
2025-04-08 00:09:37 +0200

    added placeholder image, hero section properly centered

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
         <DockNav />
       </div>
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+        <section className="w-full h-[calc(100vh-64px)] grid place-items-center">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
               <div className="flex flex-col justify-center space-y-4">
